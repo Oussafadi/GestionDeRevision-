32b6b5622269a448934b8d74f8302d13e1f3c272
Oussafadi <fadi.oussa@outlook.fr>
2023-01-25 20:56:12 +0100

    Interface Principale

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,57 +1,115 @@
 package com.example.myapplication1
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var MonthYearText : TextView
+    private lateinit var Date : LocalDate
+
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        //sohaib test
-//        val bt_matiere= findViewById<Button>(R.id.bt_matiere)
-//        bt_matiere.setOnClickListener {
-//            val intoaddmatiere=Intent(this,MatiereActivity::class.java)
-//            startActivity(intoaddmatiere)
-//        }
-//
-//
-//
-//        //test
 
-        val recyclerView : RecyclerView =findViewById(R.id.rv_revision)
-        // linear layout manager
+            //        sohaib test
+                    val bt_matiere= findViewById<Button>(R.id.bt_matiere)
+                     bt_matiere.setOnClickListener {
+                       val intoaddmatiere=Intent(this,MatiereActivity::class.java)
+                        startActivity(intoaddmatiere)
+                   }
+            //        test
+
+         Date = LocalDate.now()
+         findViews()
+         setAllViews()
+    }
+
+    private fun findViews() {
+         recyclerView =findViewById(R.id.rv_revision)
+         MonthYearText = findViewById(R.id.tv_MonthYear)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setMonthYearView() {
+        val formatter : DateTimeFormatter = DateTimeFormatter.ofPattern(" MMMM yyyy")
+        MonthYearText.setText(Date.format(formatter))
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun joursSemaine(): ArrayList<LocalDate> {
+        val days : ArrayList<LocalDate> = ArrayList()
+        var dimanche : LocalDate? = dernierJourSemaine()
+        val finSemaine : LocalDate = dimanche!!.plusWeeks(1)
+        dimanche= dimanche.plusDays(1)
+        val fin2 : LocalDate = finSemaine.plusDays(1)
+        while ( dimanche!!.isBefore(fin2)) {
+            days.add(dimanche!!)
+            dimanche = dimanche.plusDays(1)
+        }
+        return days
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun dernierJourSemaine(): LocalDate? {
+            val laSemaineDerniere : LocalDate = Date.minusWeeks(1)
+
+        while ( Date.isAfter(laSemaineDerniere)) {
+            if ( Date.dayOfWeek == DayOfWeek.SUNDAY) {
+                return Date
+            }
+            Date = Date.minusDays(1)
+        }
+        return null
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setAllViews() {
+        setMonthYearView()
+        val days : ArrayList<LocalDate> = joursSemaine()
         val layoutManager = LinearLayoutManager(this)
         recyclerView.setLayoutManager(layoutManager)
-       // recyclerView.layoutManager = layoutManager
-
-
-        var listeTaches : ArrayList<tacheRevision>?= ArrayList<tacheRevision>()
-        listeTaches?.add(tacheRevision("Projet Kotlin",false))
-        var listeTaches2 : ArrayList<tacheRevision> ?= ArrayList<tacheRevision>()
-        listeTaches2?.add(tacheRevision("Projet xml1",false))
-        var listeTaches3 : ArrayList<tacheRevision> ?= ArrayList<tacheRevision>()
-        listeTaches3?.add(tacheRevision("Projet kotlin2",false))
-        var listeTaches4 : ArrayList<tacheRevision> ?= ArrayList<tacheRevision>()
-        listeTaches4?.add(tacheRevision("Projet xml2",false))
-
-        var revisionList = listOf(
-            activiteRevision("Lundi " , listeTaches ,"01-01" ) ,
-            activiteRevision("Mardi", listeTaches2,"02-01") ,
-            activiteRevision("Mercredi", listeTaches3,"03-01"),
-            activiteRevision("Jeudi", listeTaches4,"04-01"),
-            activiteRevision("Vendredi", null,"05-01"),
-            activiteRevision("Samedi", null,"06-01"),
-            activiteRevision("Dimanche", null ,"07-01")
-
+        val Jours = listOf(
+            "Lundi",
+            "Mardi",
+            "Mercredi",
+            "Jeudi",
+            "Vendredi",
+            "Samedi",
+            "Dimanche"
         )
-        val myadapter = RevisionAdapter(revisionList)
-         recyclerView.setAdapter(myadapter)
+         val myadapter = RevisionAdapter(Jours,days)
+          recyclerView.setAdapter(myadapter)
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    public fun SemaineProchaine(view : View) {
+        Date = Date.plusWeeks(1)
+        setAllViews()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    public fun SemainePrecedante(view : View) {
+     Date = Date.minusWeeks(1)
+        setAllViews()
+    }
+
+
 
 
 
--- app/src/main/java/com/example/myapplication1/RevisionAdapter.kt
+++ app/src/main/java/com/example/myapplication1/RevisionAdapter.kt
@@ -1,20 +1,24 @@
 package com.example.myapplication1
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-class RevisionAdapter( var activiteRevisionlist: List<activiteRevision>) : RecyclerView.Adapter<RevisionAdapter.ViewHolder>(){
+class RevisionAdapter( var Jours: List<String> , var days : ArrayList<LocalDate>) : RecyclerView.Adapter<RevisionAdapter.ViewHolder>(){
 
     // provide a reference to the views for each data item
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val tv_activiteday: TextView = itemView.findViewById(R.id.tv2_day)
         val tv_activitedate: TextView = itemView.findViewById(R.id.tv2_date)
         val tv_activitename: TextView = itemView.findViewById(R.id.tv2_name)
-        val tv_activitebutton: ImageButton = itemView.findViewById(R.id.completedButton)
+         val tv_activitebutton: ImageButton = itemView.findViewById(R.id.completedButton)
 
     }
 
@@ -26,21 +30,22 @@ class RevisionAdapter( var activiteRevisionlist: List<activiteRevision>) : Recyc
     }
 
     //replace the contents of a view (invoked by the layout manager)
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // get the activite at the given position
-        val activiteRevision = activiteRevisionlist[position]
-
-        holder.tv_activiteday.text = activiteRevision.day
-        holder.tv_activitedate.text = activiteRevision.date
-        holder.tv_activitename.text = activiteRevision.taches?.get(0)?.name
+        val jour = Jours[position]
+        val datejours= days[position]
+        holder.tv_activiteday.text = jour
+        holder.tv_activitedate.text = datejours.monthValue.toString() + "-" + datejours.dayOfMonth.toString()
          // Si la tache existe on change la visibility du bouton a VISIBILE
-        if ( activiteRevision.taches?.get(0)?.name != null ) {
+       /* if ( activiteRevision.taches?.get(0)?.name != null ) {
             holder.tv_activitebutton.visibility=View.VISIBLE
         }
+        */
 
     }
     //  return la taille de la liste d'activite de revision
     override fun getItemCount(): Int {
-         return activiteRevisionlist.size
+         return Jours.size
     }
 }
