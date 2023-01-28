4af03a3fbae7a06d514b7596b2c175e1286ad59b
yassinejellouli <testyeees@gmail.com>
2023-01-27 16:56:25 -0800

    navbar lastcommit

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -43,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                     startActivity(intent)
                 }
                 R.id.stat -> {
-//mazal mat9adat
+//mazal mat9adat 3la lah t9ad mn b3d
                 }
 
             }
